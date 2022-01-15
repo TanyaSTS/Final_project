8b545fe02b14144696680ea1f4a912ed917ced6b
Tanya STS <tanya.sts@gmail.com>
2022-01-15 17:31:44 +0100

    updated tests

--- API_labirint.py
+++ API_labirint.py
@@ -1,121 +1,60 @@
 import requests
 import json
 
-
+base_url = "https://www.labirint.ru/"
 class Labirint:
     def __init__(self):
         self.base_url = "https://www.labirint.ru/"
 
-    def get_main_page(self, filter: str = ""):
-        filter = {'filter': filter}
-        res = requests.get(self.base_url, params=filter)
+    def get_main_page(self):
+        res = requests.get(self.base_url)
         status = res.status_code
-        result = ""
-        try:
-            result = res.json()
-        except:
-            result = res.text
-        return status, result
+        return status
 
-
-    def get_book_page(self, filter: str = ""):
-        filter = {'filter': filter}
-        res = requests.get(self.base_url +"books/", params=filter)
+    def get_book_page(self):
+        res = requests.get(self.base_url +"books/")
         status = res.status_code
-        result = ""
-        try:
-            result = res.json()
-        except:
-            result = res.text
-        return status, result
+        return status
 
-    def get_best_page(self, filter: str = ""):
-        filter = {'filter': filter}
-        res = requests.get(self.base_url + "best/", params=filter)
+    def get_best_page(self):
+        res = requests.get(self.base_url +"best/")
         status = res.status_code
-        result = ""
-        try:
-            result = res.json()
-        except:
-            result = res.text
-        return status, result
+        return status
 
-    def get_school_page(self, filter: str = ""):
-        filter = {'filter': filter}
-        res = requests.get(self.base_url + "best/", params=filter)
+    def get_school_page(self):
+        res = requests.get(self.base_url + "best/")
         status = res.status_code
-        result = ""
-        try:
-            result = res.json()
-        except:
-            result = res.text
-        return status, result
+        return status
 
-    def get_games_page(self, filter: str = ""):
-        filter = {'filter': filter}
-        res = requests.get(self.base_url + "games/", params=filter)
+    def get_games_page(self):
+        res = requests.get(self.base_url + "games/")
         status = res.status_code
-        result = ""
-        try:
-            result = res.json()
-        except:
-            result = res.text
-        return status, result
+        return status
 
-    def get_office_page(self, filter: str = ""):
-        filter = {'filter': filter}
-        res = requests.get(self.base_url + "office/", params=filter)
+    def get_office_page(self):
+        res = requests.get(self.base_url + "office/")
         status = res.status_code
-        result = ""
-        try:
-            result = res.json()
-        except:
-            result = res.text
-        return status, result
+        return status
 
-    def get_club_page(self, filter: str = ""):
-        filter = {'filter': filter}
-        res = requests.get(self.base_url + "club/", params=filter)
+    def get_club_page(self):
+        res = requests.get(self.base_url + "club/")
         status = res.status_code
-        result = ""
-        try:
-            result = res.json()
-        except:
-            result = res.text
-        return status, result
+        return status
 
-    def get_cabinet_page(self, filter: str = ""):
-        filter = {'filter': filter}
-        res = requests.get(self.base_url + "cabinet/", params=filter)
+    def get_cabinet_page(self):
+        res = requests.get(self.base_url + "cabinet/")
         status = res.status_code
-        result = ""
-        try:
-            result = res.json()
-        except:
-            result = res.text
-        return status, result
+        return status
 
-    def get_putorder_page(self, filter: str = ""):
-        filter = {'filter': filter}
-        res = requests.get(self.base_url + "cabinet/putorder/", params=filter)
+    def get_putorder_page(self):
+        res = requests.get(self.base_url + "cabinet/putorder/")
         status = res.status_code
-        result = ""
-        try:
-            result = res.json()
-        except:
-            result = res.text
-        return status, result
+        return status
 
-    def get_cart_page(self, filter: str = ""):
-        filter = {'filter': filter}
-        res = requests.get(self.base_url + "cart/", params=filter)
+    def get_cart_page(self):
+        res = requests.get(self.base_url + "cart/")
         status = res.status_code
-        result = ""
-        try:
-            result = res.json()
-        except:
-            result = res.text
-        return status, result
+        return status
 
     def get_list_of_books(self, filter: str = ""):
         filter = {'filter': filter}
@@ -127,3 +66,87 @@ class Labirint:
         except:
             result = res.text
         return status, result
+
+    def get_labirint_appstore(self):
+        res = requests.get("https://apps.apple.com/ru/app/%D0%BB%D0%B0%D0%B1%D0%B8%D1%80%D0%B8%D0%BD%D1%82-%D1%80%D1%83-%D0%BA%D0%BD%D0%B8%D0%B6%D0%BD%D1%8B%D0%B9-%D0%BC%D0%B0%D0%B3%D0%B0%D0%B7%D0%B8%D0%BD/id1008650482")
+        status = res.status_code
+        return status
+
+    def get_labirint_vkontakte(self):
+        res = requests.get("https://vk.com/labirint_ru")
+        status = res.status_code
+        return status
+
+    def get_labirint_google_play(self):
+        res = requests.get("https://play.google.com/store/apps/details?id=ru.labirint.android")
+        status = res.status_code
+        return status
+
+    def get_labirint_app_gallery(self):
+        res = requests.get("https://appgallery.huawei.com/app/C101184737?appId=C101184737&source=appshare&subsource=C101184737")
+        status = res.status_code
+        return status
+
+    def get_labirint_vkontakte_dety(selfs):
+        res = requests.get("https://vk.com/labirintdeti")
+        status = res.status_code
+        return status
+
+    def get_labirint_youtube(self):
+        res = requests.get("https://www.youtube.com/user/labirintruTV")
+        status = res.status_code
+        return status
+
+    def get_labirint_instagram(self):
+        res = requests.get("https://www.instagram.com/labirintru/")
+        status = res.status_code
+        return status
+
+    def get_labirint_instagram_deti(self):
+        res = requests.get("https://www.instagram.com/labirintdeti/")
+        status = res.status_code
+        return status
+    def get_labirint_fasebook(self):
+        res = requests.get("https://www.facebook.com/labirintru")
+        status = res.status_code
+        return status
+
+    def get_labirint_odnoklasniki(self):
+        res = requests.get("https://ok.ru/labirintru")
+        status = res.status_code
+        return status
+
+    def get_labirint_twitter(self):
+        res = requests.get("https://twitter.com/labirint_ru")
+        status = res.status_code
+        return status
+
+    def get_labirint_zen(self):
+        res = requests.get("https://zen.yandex.ru/labirintru")
+        status = res.status_code
+        return status
+
+    def get_labirint_telegram(self):
+        res = requests.get("https://t.me/labirintru")
+        status = res.status_code
+        return status
+
+    def get_labirint_tiktok(self):
+        res = requests.get("https://www.tiktok.com/@labirintru")
+        status = res.status_code
+        return status
+
+    def get_labirint_partners(self):
+        res = requests.get("https://partner.labirint.ru/login")
+        status = res.status_code
+        return status
+
+    def get_labirint_vakansii(self):
+        res = requests.get("https://www.labirint.org/vakansii?tab=5")
+        status = res.status_code
+        return status
+
+
+
+
+
--- part2.py
+++ part2.py
@@ -5,54 +5,54 @@ from API_labirint import Labirint
 labirint = Labirint()
 
 #1 Тест API главной страницы
-def test_get_main_page(filter=''):
-    status = labirint.get_main_page( filter)
+def test_get_main_page():
+    status = labirint.get_main_page()
     assert status == 200
 
 
 #2 Test API of the "books" page
-def test_get_books_page(filter=''):
-    status = labirint.get_book_page( filter)
+def test_get_books_page():
+    status = labirint.get_book_page()
     assert status == 200
 
 #3 Test API of the "best" page
-def test_get_best_page(filter=''):
-    status = labirint.get_best_page( filter)
+def test_get_best_page():
+    status = labirint.get_best_page()
     assert status == 200
 
 #4 Test API of the "school" page
-def test_get_school_page(filter=''):
-    status = labirint.get_school_page( filter)
+def test_get_school_page():
+    status = labirint.get_school_page()
     assert status == 200
 
 #5 Test API of the "games" page
-def test_get_games_page(filter=''):
-    status = labirint.get_games_page( filter)
+def test_get_games_page():
+    status = labirint.get_games_page()
     assert status == 200
 
 #6 Test API of the "office" page
-def test_get_office_page(filter=''):
-    status = labirint.get_office_page( filter)
+def test_get_office_page():
+    status = labirint.get_office_page()
     assert status == 200
 
 #7 Test API of the "club" page
-def test_get_club_page(filter=''):
-    status = labirint.get_club_page( filter)
+def test_get_club_page():
+    status = labirint.get_club_page()
     assert status == 200
 
 #8 Test API of the "cabinet" page
 def test_get_cabinet_page():
-    status = labirint.get_cabinet_page( filter)
+    status = labirint.get_cabinet_page()
     assert status == 200
 
 #9 Test API of the "putorder" page
-def test_get_putorder_page(filter=''):
-    status = labirint.get_putorder_page( filter)
+def test_get_putorder_page():
+    status = labirint.get_putorder_page()
     assert status == 200
 
 #10 Test API of the "cart" page
-def test_get_cart_page(filter=''):
-    status = labirint.get_cart_page( filter)
+def test_get_cart_page():
+    status = labirint.get_cart_page()
     assert status == 200
 
 #11
@@ -79,5 +79,86 @@ def test_get_all_books_with_255letter_filter(filter='мороз' * 51):
 def test_get_all_books_with_symbol_filter(filter='@#$%$$#'):
     status = labirint.get_list_of_books( filter)
     assert status != 200
+
+#16
+def test_get_labirunt_appstore():
+    status = labirint.get_labirint_appstore()
+    assert status == 200
+
+#17
+def test_get_labirint_vkontakte():
+    status = labirint.get_labirint_vkontakte()
+    assert status == 200
+
+#18
+def test_get_labirint_google_play():
+    status = labirint.get_labirint_google_play()
+    assert status == 200
+
+#19
+def test_get_labirint_app_gallery():
+    status = labirint.get_labirint_app_gallery()
+    assert status == 200
+
+#20
+def test_get_labirint_vkontakte_deti():
+    status = labirint.get_labirint_vkontakte_dety()
+    assert status == 200
+
+#21
+def test_get_labirint_youtube():
+    status = labirint.get_labirint_youtube()
+    assert status == 200
+
+#22
+def test_get_labirint_instagram():
+    status = labirint.get_labirint_instagram()
+    assert status == 200
+
+#23
+def test_get_labirint_instagram_deti():
+    status = labirint.get_labirint_instagram_deti()
+    assert status == 200
+
+#24
+def test_get_labirint_facebook():
+    status = labirint.get_labirint_fasebook()
+    assert status == 200
+
+#25
+def test_get_labirint_odnoklassniki():
+    status = labirint.get_labirint_odnoklasniki()
+    assert status
+
+#26
+def test_get_labirint_twitter():
+    status = labirint.get_labirint_twitter()
+    assert status
+
+#27
+def test_get_labirint_zen():
+    status = labirint.get_labirint_zen()
+    assert status
+
+#28
+def test_get_labirint_telegram():
+    status = labirint.get_labirint_telegram()
+    assert status
+
+#29
+def test_get_labirint_tiktok():
+    status = labirint.get_labirint_tiktok()
+    assert status
+
+#30
+def test_get_labirint_partners():
+    status = labirint.get_labirint_partners()
+    assert status
+
+#31
+def test_get_labirint_vakansii():
+    status = labirint.get_labirint_vakansii()
+    assert status
+
 
 
